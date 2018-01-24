3e2f050fbd748f1f68a72677d3610d6a06c888f2
Thomas Isberg <thomas@tintonic.com>
2018-01-24 19:31:11 +0100

    Source folder for WIP

--- dev/example/gulpfile.js
+++ dev/example/gulpfile.js
@@ -1,18 +1,18 @@
 var gulp = require('gulp');
 var mainBowerFiles = require('main-bower-files');
 var concat = require('gulp-concat');
-// var rename = require('gulp-rename');
-// var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 
 var dest = '../../public/example/';
 
 function js() {
     return gulp.src(mainBowerFiles())
         .pipe(concat('scripts.js'))
+        .pipe(gulp.dest(dest))
+        .pipe(rename('scripts.min.js'))
+        .pipe(uglify())
         .pipe(gulp.dest(dest));
-        // .pipe(rename('scripts.min.js'))
-        // .pipe(uglify())
-        // .pipe(gulp.dest(dest));
 }
 
 function dist() {
