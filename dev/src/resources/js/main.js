15f39af093547badadf800577292ed97b28b5972
Thomas Isberg <thomas@tintonic.com>
2018-03-15 11:22:02 +0100

    Removed some stuff

--- dev/src/resources/js/main.js
+++ dev/src/resources/js/main.js
@@ -3,6 +3,5 @@ import TinGrid from './tin-grid';
 TinGrid(document.getElementById('TinGrid'), {
     itemHeightType: "ratio",
     itemHeight: 1,
-    wideItemHeight: 9/16,
     useTransition: true
 });
